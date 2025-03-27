ae99860279840ddb140c1fa68c59eafd3d366b34
mani <ganoglamanikanta@gmail.com>
2025-03-27 19:48:00 +0530

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,27 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './App.css'
-import Locate from './location/locate'
-import First from './home/first'
-import Second from './home/second'
-import Show from './Timeline/show'
-import Calu from './Caluculate'
-import Food from './Timeline/food'
-
-const router = createBrowserRouter([
-  {
-    path:"/map",
-    element:<Locate/>
-  },
-  {
-    path:"/first",
-    element:<First/>
-  },
-  {
-    path:"/second",
-    element:<Second/>
-  },
-  {
-    path:"/show",
-    element:<Show/>
-  },
-  {
-    path:"/cal",
-    element:<Calu/>
-  }, {
-    path:"/food",
-    element:<Food/>
-  }
-])
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Locate from './location/locate';
+import First from './home/first';
+import Second from './home/second';
+import Show from './Timeline/show';
+import Calu from './Caluculate';
+import Food from './Timeline/food';
 
 function App() {
   return (
     <main>
-      <RouterProvider router={router}></RouterProvider>
+      <HashRouter>
+        <Routes>
+          <Route path="/map" element={<Locate />} />
+          <Route path="/first" element={<First />} />
+          <Route path="/second" element={<Second />} />
+          <Route path="/show" element={<Show />} />
+          <Route path="/cal" element={<Calu />} />
+          <Route path="/food" element={<Food />} />
+        </Routes>
+      </HashRouter>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
